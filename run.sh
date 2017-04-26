fa6da7b1a5e64466423fdb5886f785850218c72d
bridge wang <tigerfan7495@gmail.com>
2017-04-26 09:43:43 -0400

    Update run.sh
     preload caMicroscope Template document

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #nohup /usr/bin/mongod &
 mongod --config /etc/mongod.conf &
+#preload caMicroscope Template document
+mongo < /root/bindaas/bin/loadCamicroscopeTemplate.js
 sh startup.sh &
 #mongod
 while true; do sleep 1000; done
-#preload caMicroscope Template document
-mongo < loadCamicroscopeTemplate.js
+
