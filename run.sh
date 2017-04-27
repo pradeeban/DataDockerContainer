b9ed84bf50eb49c5462beebec420a25076b380c7
bridge wang <tigerfan7495@gmail.com>
2017-04-27 10:48:54 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,15 +4,3 @@ mongod --config /etc/mongod.conf &
 sh startup.sh &
 #mongod
 while true; do sleep 1000; done
-
-#pre-load Camicroscope Template document
-runingMongod=$(ps -a |grep mongod)
-#echo ${#runingMongod}
-tmplength=${#runingMongod}
-while [ $tmplength -eq 0 ] ; 
-   do  echo "mongod is not runing." && 
-       sleep 1000 && 
-       runingMongod=$(ps -a |grep mongod) &&
-       tmplength=${#runingMongod}       
-done
-mongo < /root/bindaas/bin/loadCamicroscopeTemplate.js
