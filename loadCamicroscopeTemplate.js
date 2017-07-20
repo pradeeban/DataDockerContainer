3abb0bf2cfeed44d8f2adeefdee71a569158ed45
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-07-20 17:06:40 -0400

    Fixes #188

--- loadCamicroscopeTemplate.js
+++ loadCamicroscopeTemplate.js
@@ -1,29 +1,30 @@
 print("-- start script here --");
 use caMicroscope_Templates;
-var AnnotationTemplate_records = db.AnnotationTemplate.find().count();
-if(AnnotationTemplate_records == 0){
-db.AnnotationTemplate.insert({ 
-      "_id" : ObjectId("5789471e80e00235a16436a3"),
-     "region" : {
-        "enum" : [
-            "Good Segmentation",
-            "Clump"
-        ], 
-        "title" : "Region:", 
-        "type" : "string"
-    }, 
-    "additional_annotation" : {
-        "title" : "Additional annotation for the region: ", 
-        "type" : "textarea"
-    }, 
-    "additional_notes" : {
-        "title" : "Additional notes: ", 
-        "type" : "textarea"
-    }, 
-    "secret" : {
-        "title" : "Secret: ", 
-        "type" : "password"
-    }
- })
-}
+db.AnnotationTemplate.update(
+    { "_id": ObjectId("5789471e80e00235a16436a3") },
+    {
+        "_id": ObjectId("5789471e80e00235a16436a3"),
+        "region": {
+            "enum": [
+                "Good Segmentation",
+                "Clump"
+            ],
+            "title": "Region:",
+            "type": "string"
+        },
+        "additional_annotation": {
+            "title": "Additional annotation for the region: ",
+            "type": "textarea"
+        },
+        "additional_notes": {
+            "title": "Additional notes: ",
+            "type": "textarea"
+        },
+        "secret": {
+            "title": "Secret: ",
+            "type": "password"
+        }
+    },
+    { "upsert": true }
+);
 print("-- end of  script  --");
