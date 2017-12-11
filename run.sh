cf552e8716acb9b7c41fe46cdfd460eed0cf81f1
bridge2014 <feiqiao@tahsin175.informatics.stonybrook.edu>
2017-12-11 11:55:37 -0500

    create new branch

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ sh /root/bindaas/bin/startup.sh &
 #preload camicroscope template
 sleep 5
 mongo 127.0.0.1:27017/quip /root/bindaas/bin/db_index.js
+mongo 127.0.0.1:27017/quip_comp /root/bindaas/bin/db_index.js
 mongo < /root/bindaas/bin/loadCamicroscopeTemplate.js
 mongo < /root/bindaas/bin/load_admin_info.js
 while true; do sleep 1000; done
