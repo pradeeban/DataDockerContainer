c247ebc6a0ddcfc58a80a39a97b4316a7a3a6cdd
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2019-03-08 14:03:31 -0500

    Remove unused project files

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-#nohup /usr/bin/mongod &
 mongod --config /etc/mongod.conf &
 sh /root/bindaas/bin/startup.sh &
-#preload camicroscope template
-sleep 5
-mongo 127.0.0.1:27017/quip /root/bindaas/bin/db_index.js
-mongo 127.0.0.1:27017/quip_comp /root/bindaas/bin/db_index.js
-mongo < /root/bindaas/bin/loadCamicroscopeTemplate.js
-mongo < /root/bindaas/bin/load_admin_info.js
 while true; do sleep 1000; done
 
